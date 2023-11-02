3e1ffe6f939a514f6ffd5010cbc458c5e1d6adfe
dev-com2020 <kwp@tlen.pl>
2023-11-02 14:31:03 +0100

    14:31 02.11

--- dzien1e.py
+++ dzien1e.py
@@ -19,3 +19,14 @@ zakupy_do_usuniecia = 'piwo'
 zakupy = [i for i in zakupy if i != zakupy_do_usuniecia]
 
 print(zakupy)
+
+for index, wartosc in enumerate(lista):
+    print(f"index: {index} wartosc: {wartosc}")
+    # print("index: %i wartosc: %i " % (index, wartosc))
+
+wersja = 3.10
+jezyk = "C++"
+print("Używam %s w wersji %i" % (jezyk, wersja))
+print("Używam pythona w wersji %f" % wersja)
+print("Używam pythona w wersji %.2f" % wersja)
+print("Używam {0} w wersji {1}".format(jezyk, wersja))
