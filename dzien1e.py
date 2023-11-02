29a22fb783e3f345dec9333d56dcb06d34ed0c6a
dev-com2020 <kwp@tlen.pl>
2023-11-02 14:54:13 +0100

    14:54 02.11

--- dzien1e.py
+++ dzien1e.py
@@ -30,3 +30,40 @@ print("Używam %s w wersji %i" % (jezyk, wersja))
 print("Używam pythona w wersji %f" % wersja)
 print("Używam pythona w wersji %.2f" % wersja)
 print("Używam {0} w wersji {1}".format(jezyk, wersja))
+
+spec_string = """
+    to jest tekst...🤩
+        to jest tekst...🤩
+            to jest tekst...🤩"""
+
+print(spec_string)
+
+krotka = ("Ania", "Gosia", "Jacek", "Adam")
+imie1, *imie2, imie3 = krotka
+print(imie1)
+print(imie2)
+print(imie3)
+
+liczby = range(2, 6, 2)
+lista = list(liczby)
+print(liczby)
+
+for i in liczby:
+    print(i)
+
+print(lista)
+
+liczba = int(input("Podaj liczbę: "))
+
+if liczba < 10:
+    print("Liczba jest mniejsza od 10")
+elif liczba == 10:
+    print("Liczba to 10!")
+    pytanie = input("Czy lubisz koty? (t/n)")
+    if pytanie == 't':
+        print("🐈")
+    else:
+        print("🐀")
+else:
+    print("Liczba jest większa od 10")
+
