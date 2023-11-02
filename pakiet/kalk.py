e7a514408e0431d8ee4c6ef7362f46efa870a502
dev-com2020 <kwp@tlen.pl>
2023-11-02 15:30:12 +0100

    15:30 02.11

---
+++ pakiet/kalk.py
@@ -0,0 +1,22 @@
+def dodaj(a, b):
+    """
+    Jest to funkcja dodająca a i b
+    :param a: int float
+    :param b: int float
+    :return: suma liczba a,b
+    """
+    return a + b
+
+
+dodaj2 = lambda a, b: a + b
+
+
+def odejmij(a, b):
+    return a - b
+
+
+def pomnoz(a, b):
+    return a * b
+
+
+dziel = lambda a, b: a / b
---
+++ start.py
@@ -0,0 +1,7 @@
+from pakiet import kalk as k
+
+print(k.dodaj(4, 5))
+print(k.odejmij(4, 5))
+print(k.pomnoz(4, 5))
+print(k.dodaj2(5, 5))
+print(k.dziel(50, 2))
