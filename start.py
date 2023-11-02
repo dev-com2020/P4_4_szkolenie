e7bcef0e91375ad3238a19d536e86baa3fe4969c
dev-com2020 <kwp@tlen.pl>
2023-11-02 15:38:40 +0100

    15:38 02.11

--- start.py
+++ start.py
@@ -5,3 +5,15 @@ print(k.odejmij(4, 5))
 print(k.pomnoz(4, 5))
 print(k.dodaj2(5, 5))
 print(k.dziel(50, 2))
+
+
+def suma(*x):
+    if len(x) == 0:
+        return "nie mam czego sumować"
+    else:
+        return sum(x)
+
+
+print(suma())
+print(suma(5, 5))
+print(suma(5, 5, 10, 50))
