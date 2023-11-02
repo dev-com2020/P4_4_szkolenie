12961083cecde5ecccb764a596f0ed7e93632017
dev-com2020 <kwp@tlen.pl>
2023-11-02 13:47:18 +0100

    13:47 02.11

--- dzien1d.py
+++ dzien1d.py
@@ -12,3 +12,27 @@ else:
 print(x)
 print(y)
 print(z)
+
+licznik = 0
+while True:
+    licznik += 1  # licznik = licznik + 1
+    if licznik > 10:
+        break
+
+print(licznik)
+
+counter = 0
+while counter < 5:
+    print("Licznik wynosi:", counter)
+    counter += 1
+
+lista = []
+print("Podaj liczbę, którą chcesz umieścić w liście")
+print("Wpisz 'stop' aby zakończyć program")
+while True:
+    wejscie = input()
+    if wejscie == 'stop':
+        break
+    lista.append(int(wejscie))
+
+print("Twoja lista ->", lista)
