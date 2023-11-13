7e768a6db68671e0ce0e5c38fe82326d431ff750
DEV-COM <kwp@tlen.pl>
2023-11-13 10:46:02 +0100

    10:45 13.11

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from my_app.hello.views import hello
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 app.config.from_pyfile('setup.cfg')
 # app.config.from_object('config.ProductionConfig')
-app.register_blueprint(hello)
+# app.register_blueprint(hello)
--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -3,6 +3,8 @@ from my_app.hello.models import MESSAGES
 
 hello = Blueprint('hello', __name__)
 
+
+@hello.route('/')
 @hello.route('/hello')
-def hello():
+def hello_w():
     return MESSAGES['default']
