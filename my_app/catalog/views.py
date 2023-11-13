46bf24ad2467a6f082dede40e21a52e79e0b3f5a
DEV-COM <kwp@tlen.pl>
2023-11-13 15:12:15 +0100

    15:12 13.11

--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, request
 from my_app import db
-from my_app.catalog.models import Product
+from my_app.catalog.models import Product, Category
 
 catalog = Blueprint('catalog', __name__)
+
 
 @catalog.route('/')
 @catalog.route('/catalog')
@@ -12,7 +13,8 @@ def products():
     for product in products:
         res[product.id] = {
             'name': product.name,
-            'price': str(product.price)
+            'price': str(product.price),
+            'category': product.category.name
         }
     return jsonify(res)
 
@@ -21,13 +23,44 @@ def products():
 def create_product():
     name = request.form.get('name')
     price = request.form.get('price')
-    product = Product(name, price)
+    categ_name = request.form.get('category')
+    category = Category.query.filter_by(name=categ_name).first()
+    if not category:
+        category = Category(categ_name)
+
+    product = Product(name, price, category)
     db.session.add(product)
     db.session.commit()
     return 'Product added'
+
+
+@catalog.route('/category-create', methods=['POST'])
+def create_category():
+    name = request.form.get('name')
+    category = Category(name)
+    db.session(category)
+    db.session.commit()
+    return 'Kategoria dodana'
 
 
 @catalog.route('/product/<id>')
 def product(id):
     product = Product.query.get_or_404(id)
     return 'Produkt - %s, PLN %s' % (product.name, product.price)
+
+
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    res = {}
+    for c in categories:
+        res[c.id] = {
+            'name': c.name,
+        }
+        for product in c.products:
+            res[c.id]['products'] = {
+                'id': product.id,
+                'name': product.name,
+                'price': product.price
+            }
+    return jsonify(res)
