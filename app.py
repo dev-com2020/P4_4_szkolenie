a3a12a6d9a7b99592fb82baf0e37ba84312bd457
DEV-COM <kwp@tlen.pl>
2023-11-13 09:50:26 +0100

    09:50 13.11

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_pyfile('setup.cfg')
 
 @app.route('/')
 def hello_world():
-    return 'Witaj Flasku!'
+    return 'Witaj Flasku...'
 
 
 if __name__ == '__main__':
     app.run()
+
+
+# dla mac'a w terminalu przed uruchomieniem serwera
+# export FLASK_APP=app.py
+# python3 -m flask run
