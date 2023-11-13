99b18eba1e2fc16aa28eac96a9bcd1820c88ae6b
DEV-COM <kwp@tlen.pl>
2023-11-13 14:09:40 +0100

    14:09 13.11

--- my_app/product/views.py
+++ my_app/product/views.py
@@ -18,3 +18,16 @@ def product(key):
     if not prod:
         abort(404)
     return render_template('product.html', product=prod)
+
+
+@product_blueprint.context_processor
+def some_processor():
+    def full_name(product):
+        return '{0} / {1}'.format(product['category'], product['name'])
+
+    return {'full_name': full_name}
+
+
+@product_blueprint.app_template_filter('full_name')
+def full_name_filter(product):
+    return '{0} / {1}'.format(product['category'], product['name'])
