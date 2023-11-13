dfd976ad98286198b08e109b80f006ffe902e001
DEV-COM <kwp@tlen.pl>
2023-11-13 13:48:48 +0100

    13:48 13.11

--- my_app/product/views.py
+++ my_app/product/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from werkzeug.exceptions import abort
 
 from my_app.product.models import PRODUCTS
 
@@ -9,3 +10,11 @@ product_blueprint = Blueprint('product', __name__)
 @product_blueprint.route('/home')
 def home():
     return render_template('home.html', products=PRODUCTS)
+
+
+@product_blueprint.route('/product/<key>')
+def product(key):
+    prod = PRODUCTS.get(key)
+    if not prod:
+        abort(404)
+    return render_template('product.html', product=prod)
