748bf738f77ba34bb738302d90596799c5185d60
DEV-COM <kwp@tlen.pl>
2023-11-14 14:51:32 +0100

    14:51 14.11

---
+++ mat_flask/example1.py
@@ -0,0 +1,33 @@
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def get_plot():
+    data = {
+        'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)
+    }
+    data['b'] = data['a'] + 10 * np.random.randn(50)
+    data['d'] = np.abs(data['d']) * 100
+
+    plt.scatter('a', 'b', c='c', s='c', data=data)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    return plt
+
+
+@app.get('/')
+def single_converter():
+    plot = get_plot()
+    plot.savefig(os.path.join('static', 'images', 'plot.png'))
+    return render_template('matplotlib-plot1.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
